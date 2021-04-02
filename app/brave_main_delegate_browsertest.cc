6bb31df40e0284919209fe1198a6df640d1ffd00
mkarolin <max@brave.com>
2021-04-02 18:00:29 -0400

    SafeBrowsingEnhancedProtection flag was removed.
    EnhancedProtectionMessageInInterstitials flag was removed as well.
    
    Chromium changes:
    
    https://chromium.googlesource.com/chromium/src/+/e468da6e701cc0c0a55778ba13e934a5539e16b1
    
    commit e468da6e701cc0c0a55778ba13e934a5539e16b1
    Author: Xinghui Lu <xinghuilu@chromium.org>
    Date:   Wed Mar 24 22:45:50 2021 +0000
    
        Remove the SafeBrowsingEnhancedProtection flag.
    
        Enhanced protection is launched since M86.
    
        Bug: 1097310
    
    https://chromium.googlesource.com/chromium/src/+/e84b72555a6afccaacdf341545ed09695355b535
    
    commit e84b72555a6afccaacdf341545ed09695355b535
    Author: Xinghui Lu <xinghuilu@chromium.org>
    Date:   Tue Mar 23 07:54:05 2021 +0000
    
        Remove the EnhancedProtectionMessageInInterstitials flag.
    
        This is launched since M87.
    
        Bug: 1130721

--- app/brave_main_delegate_browsertest.cc
+++ app/brave_main_delegate_browsertest.cc
@@ -18,7 +18,6 @@
 #include "components/network_time/network_time_tracker.h"
 #include "components/omnibox/common/omnibox_features.h"
 #include "components/password_manager/core/common/password_manager_features.h"
-#include "components/safe_browsing/core/features.h"
 #include "components/security_state/core/features.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/common/content_features.h"
@@ -78,8 +77,6 @@ IN_PROC_BROWSER_TEST_F(BraveMainDelegateBrowserTest, DisabledFeatures) {
       &features::kWebOTP,
       &kSharingQRCodeGenerator,
       &network_time::kNetworkTimeServiceQuerying,
-      &safe_browsing::kEnhancedProtection,
-      &safe_browsing::kEnhancedProtectionMessageInInterstitials,
   };
 
   for (const auto* feature : disabled_features)
